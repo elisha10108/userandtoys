56729d742f14b9ea14e7d14592c36d25cbf9f47b
elisha <elisha10108@gmail.com>
2021-02-25 00:19:54 +0200

    master

--- config/secretData.js
+++ config/secretData.js
@@ -1,8 +1,8 @@
 // כל מה שסודי כמו סיסמא , יוזר יהיה כאן
 exports.config = {
-  jwtSecret:"elisha",
-  mongoPass:"elisha",
-  mongoUser:"elisha"
+  jwtSecret:"",
+  mongoPass:"",
+  mongoUser:""
 }
 
 
